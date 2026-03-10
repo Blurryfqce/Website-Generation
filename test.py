d8f27f70063c5418d0e504fee52cb5f3d9b13824
blurryfqce <ajaoawwal05@gmail.com>
2026-03-10 11:33:07 +0100

    port

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ data = {
     "message": message,
     "session_id": " " #Input random alphanumeric value here as user id 
 }
-response = requests.post("http://127.0.0.1:5000/build", json= data)
+response = requests.post("https://website-generator-s8u3.onrender.com", json= data)
 
 print(response.json())
--- web.py
+++ web.py
@@ -68,5 +68,5 @@ def build():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug= True)
-    # app.run(debug=False, host="0.0.0.0", port=5000)
+    # app.run(debug= True)
+    app.run(debug=False, host="0.0.0.0", port=5000)
