77e8c83400162d66c60b638b9c09288150df42f1
blurryfqce <ajaoawwal05@gmail.com>
2026-03-10 11:39:36 +0100

    Update to hosted api

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ data = {
     "message": message,
     "session_id": " " #Input random alphanumeric value here as user id 
 }
-response = requests.post("https://website-generator-s8u3.onrender.com", json= data)
+response = requests.post("https://website-generator-s8u3.onrender.com/build", json= data)
 
 print(response.json())
